2326774d01ec04633604c5565b6278076b442e75
MaperPario <69398573+MaperPario@users.noreply.github.com>
2021-05-11 19:47:58 -0700

    change port to 80

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 const data = require('./data.json');
-const port = 3000;
+const port = 80;
 
 //set view engine to pug templating engine
 app.set('view engine', 'pug');
